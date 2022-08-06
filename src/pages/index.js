df9e47d110c6f38303fda7956003b34581d31f0d
euxzy <rijal.m12l@gmail.com>
2022-08-06 16:38:03 +0700

    setup project

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="text-3xl">Hello World!</div>
+    </div>
+  );
+};
+
+export default Home;
